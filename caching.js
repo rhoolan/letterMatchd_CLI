7f4a2fe65c3f2e877d3d11427824ef69397bd1c0
Rob Hoolan <68448185+rhoolan@users.noreply.github.com>
2024-08-22 05:40:49 +0000

    Move score caching to caching.js

--- caching.js
+++ caching.js
@@ -92,9 +92,59 @@ async function writeCacheToFile(filePath, cache, cacheName) {
   }
 }
 
+// Function to read the cache file and populate the cache object with user-user scores
+// {user-user : score : date}
+async function readInScoresFromFile(filePath, scoreCache) {
+  try {
+    const data = await fs.readFile(filePath, "utf8");
+    const lines = data.split("\n");
+
+    lines.forEach((line) => {
+      const info = line.split(" : ");
+      const userNames = info[0];
+      const score = info[1];
+      const date = info[2];
+
+      scoreCache.set(userNames, { score, date });
+    });
+
+    return scoreCache; // Return the cache here
+  } catch (error) {
+    console.log("Could not read file:", error);
+    throw error;
+  }
+}
+
+// Function to write the cache to the txt file
+// CURRENT STATE JUST WRITES BLANK FILE
+async function writeScoresToFile(filePath, cache, cacheName) {
+  try {
+    // Get the Map object from the CacheWithExpiry instance
+    const cacheMap = cache.cache;
+    // Get size of cache for tracking if an item is the last or not
+    const size = cacheMap.size;
+
+    // Prepare the data to write
+    const data = Array.from(cacheMap.entries())
+      .map(([key, { value, expiryDate }], index) => {
+        // Check if it's the last item, omit the line break
+        return `${key} : ${value} : ${expiryDate}${index !== size - 1 ? "\n" : ""}`;
+      })
+      .join("");
+
+    // Write all data at once
+    await fs.writeFile(filePath, data, "utf8"); // Specify encoding
+    console.log(`\n${cacheName} data written to cache`);
+  } catch (error) {
+    console.error("Failed to write data to cache:", error);
+  }
+}
+
 // Export the readCacheFile function
 module.exports = {
   readInCacheFromFile,
   writeCacheToFile,
   CacheWithExpiry,
+  readInScoresFromFile,
+  writeScoresToFile,
 };
